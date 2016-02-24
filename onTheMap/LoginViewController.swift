07ec2046f40dda0ab9a10d5d33fcca5f461bca06
mcberros <mcberros@gmail.com>
2016-02-24 15:59:43 +0100

    Added sign up action and unwrapping of url

--- onTheMap/LoginViewController.swift
+++ onTheMap/LoginViewController.swift
@@ -17,6 +17,7 @@ class LoginViewController: UIViewController {
 
     let baseUdacityURL = "https://www.udacity.com/"
     let sessionMethod = "api/session"
+    let signUpMethod = "account/auth#!/signup"
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,6 +32,13 @@ class LoginViewController: UIViewController {
         }
     }
 
+    @IBAction func signUpButtonTouch(sender: AnyObject) {
+        let urlString = baseUdacityURL + signUpMethod
+        if let url = NSURL(string: urlString) {
+            UIApplication.sharedApplication().openURL(url)
+        }
+    }
+
     private func showAlert(message: String) {
         let alertController = UIAlertController(title: nil, message: message, preferredStyle: .Alert)
         let dismissAction = UIAlertAction(title: "Dismiss", style: .Cancel) {(_) in }
@@ -41,39 +49,39 @@ class LoginViewController: UIViewController {
 
     private func getSession() {
         let urlString = baseUdacityURL + sessionMethod
-        let url = NSURL(string: urlString)
-        let request = NSMutableURLRequest(URL: url!)
+        if let url = NSURL(string: urlString) {
+            let request = NSMutableURLRequest(URL: url)
 
-        request.HTTPMethod = "POST"
-        request.addValue("application/json", forHTTPHeaderField: "Accept")
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.HTTPBody = "{\"udacity\": {\"username\": \"\(userTextField.text!)\", \"password\": \"\(passwordTextField.text!)\"}}".dataUsingEncoding(NSUTF8StringEncoding)
+            request.HTTPMethod = "POST"
+            request.addValue("application/json", forHTTPHeaderField: "Accept")
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.HTTPBody = "{\"udacity\": {\"username\": \"\(userTextField.text!)\", \"password\": \"\(passwordTextField.text!)\"}}".dataUsingEncoding(NSUTF8StringEncoding)
 
-        let session = NSURLSession.sharedSession()
-        let task = session.dataTaskWithRequest(request) { data, response, error in
-            guard (error == nil) else {
-                dispatch_async(dispatch_get_main_queue()){self.showAlert("Connection error")}
-                return
-            }
-
-            guard let statusCode = (response as? NSHTTPURLResponse)?.statusCode where statusCode >= 200 && statusCode <= 299 else {
-                print(response)
-                if (response as? NSHTTPURLResponse)?.statusCode == 403 {
-                    dispatch_async(dispatch_get_main_queue()){self.showAlert("Invalid email or password")}
-                } else {
-                    dispatch_async(dispatch_get_main_queue()){self.showAlert("Your request returned an invalid response")}
+            let session = NSURLSession.sharedSession()
+            let task = session.dataTaskWithRequest(request) { data, response, error in
+                guard (error == nil) else {
+                    dispatch_async(dispatch_get_main_queue()){self.showAlert("Connection error")}
+                    return
                 }
-                return
-            }
 
-            guard let data = data else {
-                dispatch_async(dispatch_get_main_queue()){self.showAlert("No data was returned by the request")}
-                return
-            }
+                guard let statusCode = (response as? NSHTTPURLResponse)?.statusCode where statusCode >= 200 && statusCode <= 299 else {
+                    if (response as? NSHTTPURLResponse)?.statusCode == 403 {
+                        dispatch_async(dispatch_get_main_queue()){self.showAlert("Invalid email or password")}
+                    } else {
+                        dispatch_async(dispatch_get_main_queue()){self.showAlert("Your request returned an invalid response")}
+                    }
+                    return
+                }
 
-            let newData = data.subdataWithRange(NSMakeRange(5, (data.length) - 5))
-            print(NSString(data: newData, encoding: NSUTF8StringEncoding))
+                guard let data = data else {
+                    dispatch_async(dispatch_get_main_queue()){self.showAlert("No data was returned by the request")}
+                    return
+                }
+
+                let newData = data.subdataWithRange(NSMakeRange(5, (data.length) - 5))
+                print(NSString(data: newData, encoding: NSUTF8StringEncoding))
+            }
+            task.resume()
         }
-        task.resume()
     }
 }
