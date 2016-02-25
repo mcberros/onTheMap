3082bfd672e5334b0dfe5d21e2ad4ce91315e50c
mcberros <mcberros@gmail.com>
2016-02-25 19:57:30 +0100

    Added logout

--- onTheMap/TabBarController.swift
+++ onTheMap/TabBarController.swift
@@ -10,8 +10,10 @@ import UIKit
 
 class TabBarController: UITabBarController {
 
+    var appDelegate: AppDelegate!
+
     @IBAction func logoutButtonItemTouch(sender: AnyObject) {
-        self.dismissViewControllerAnimated(true, completion: nil)
+        logout()
     }
 
     @IBAction func pinButtonItemTouch(sender: AnyObject) {
@@ -20,5 +22,76 @@ class TabBarController: UITabBarController {
 
 
     @IBAction func refreshButtonItemTouch(sender: AnyObject) {
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+    }
+
+    private func logout() {
+        let urlString = ApisClient.Constants.BaseUdacityURL + ApisClient.Methods.SessionMethod
+        if let url = NSURL(string: urlString) {
+            let request = NSMutableURLRequest(URL: url)
+
+            request.HTTPMethod = "DELETE"
+
+            var xsrfCookie: NSHTTPCookie? = nil
+            let sharedCookieStorage = NSHTTPCookieStorage.sharedHTTPCookieStorage()
+            for cookie in sharedCookieStorage.cookies! {
+                if cookie.name == "XSRF-TOKEN" { xsrfCookie = cookie }
+            }
+
+            if let xsrfCookie = xsrfCookie {
+                request.setValue(xsrfCookie.value, forHTTPHeaderField: "X-XSRF-TOKEN")
+            }
+
+            let session = NSURLSession.sharedSession()
+            let task = session.dataTaskWithRequest(request) { data, response, error in
+                guard (error == nil) else{
+                    dispatch_async(dispatch_get_main_queue()){self.appDelegate.showAlert(self, message: "Connection error")}
+                    return
+                }
+
+                guard let statusCode = (response as? NSHTTPURLResponse)?.statusCode where statusCode >= 200 && statusCode <= 299 else {
+                    dispatch_async(dispatch_get_main_queue()){self.appDelegate.showAlert(self, message: "Your request returned an invalid response")}
+                    return
+                }
+
+                guard let data = data else {
+                    dispatch_async(dispatch_get_main_queue()){self.appDelegate.showAlert(self, message: "No data was returned by the request")}
+                    return
+                }
+
+                let newData = data.subdataWithRange(NSMakeRange(5, data.length - 5))
+
+                let parsedResult: AnyObject!
+                do {
+                    parsedResult = try NSJSONSerialization.JSONObjectWithData(newData, options: .AllowFragments)
+                } catch {
+                    dispatch_async(dispatch_get_main_queue()){self.appDelegate.showAlert(self, message: "The logout response data could not be parsed")}
+                    return
+                }
+
+                guard let _ = parsedResult["session"]!!["id"] as? String else{
+                    dispatch_async(dispatch_get_main_queue()){self.appDelegate.showAlert(self, message: "There was an error in the parsed data for the logout")}
+                    return
+                }
+
+                print(NSString(data: newData, encoding: NSUTF8StringEncoding))
+
+                self.completeLogout()
+            }
+            task.resume()
+
+        }
+
+    }
+
+    private func completeLogout() {
+        dispatch_async(dispatch_get_main_queue()) {
+            self.dismissViewControllerAnimated(true, completion: nil)
+        }
     }
 }
